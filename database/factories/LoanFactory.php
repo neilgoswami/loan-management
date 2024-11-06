2c5f7e9c328d920b86a6f4ac67508cd818e5b710
Neel Goswami <neelgoswami.cs@gmail.com>
2024-11-06 00:25:39 -0800

    chore: added loan seeder and factory
    Load seeder and factory will help us generate test data. Artisan command to generate test loans `php artisan db:seed --class=LoanSeeder`

---
+++ database/factories/LoanFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Loan>
+ */
+class LoanFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'loan_amount' => $this->faker->numberBetween(1000, 100000),
+            'interest_rate' => $this->faker->randomFloat(2, 1, 20),
+            'loan_duration' => $this->faker->numberBetween(12, 120),
+        ];
+    }
+}
