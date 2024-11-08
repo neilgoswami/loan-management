58afba81f8d20d0d123aa1e50f63cdb2f7f047d0
Neel Goswami <neelgoswami.cs@gmail.com>
2024-11-07 22:13:46 -0800

    feat: revoke auth token
    For the time been, token has all abilities and expiration is of one day.

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\API\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->get('/logout', [AuthController::class, 'logout']);
