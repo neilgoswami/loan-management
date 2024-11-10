fade2293dd15a977136e60366abecc9e7c4ad6a6
Neel Goswami <neelgoswami.cs@gmail.com>
2024-11-10 03:24:43 -0800

    feat: remove auth requirement for loan retrieval apis

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -3,4 +3,8 @@
 use App\Http\Controllers\API\V1\LoanController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->apiResource('loans', LoanController::class);
+// No authentication required for loan retrieval APIs
+Route::get('loans', [LoanController::class, 'index'])->name('loans.index');
+Route::get('loans/{loan}', [LoanController::class, 'show'])->name('loans.show');
+
+Route::middleware('auth:sanctum')->apiResource('loans', LoanController::class)->except(['show', 'index']);
